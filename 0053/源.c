9b3beccc005a54a757b65e05d56f2bcf805a6a9c
12345 <67890>
2019-12-27 17:07:41 +0800

    0053.3 动态规划
    O(n)

--- 0053/源.c
+++ 0053/源.c
@@ -8,40 +8,23 @@
 #define true 1
 #define false 0
 
-
-int func(int* nums, int left, int right) {
-	if (left > right)
-		return INT_MIN;
-
-	int sum;
-	int mid = (left + right) / 2;
-
-	int max_left = func(nums, left, mid - 1);
-
-	int max_right = func(nums, mid + 1, right);
-
-	int max_midl = 0;
-	sum = 0;
-	for (int i = mid - 1; i >= left; i--) {
-		sum += nums[i];
-		max_midl = sum > max_midl ? sum : max_midl;
-	}
-	int max_midr = 0;
-	sum = 0;
-	for (int i = mid + 1; i <= right; i++) {
-		sum += nums[i];
-		max_midr = sum > max_midr ? sum : max_midr;
-	}
-	int max_mid = max_midl + max_midr + nums[mid];
-	return max(max_left, max(max_mid, max_right));
+int max_y(int a, int b) {
+	return a > b ? a : b;
 }
 
+
 int maxSubArray(int* nums, int numsSize) {
-	return func(nums, 0, numsSize - 1);
+	int max_s = nums[0];
+	int dp_i = nums[0];
+	for (int i = 1; i < numsSize; i++) {
+		dp_i = max_y(dp_i + nums[i], nums[i]);
+		max_s = max_y(dp_i, max_s);
+	}
+	return max_s;
 }
 
 int main() {
-	int nums[] = { -2,1,-3,4,-1,2,1,-5,4 };
+	int nums[] = {1 };
 	int sum = maxSubArray(nums, sizeof(nums) / sizeof(nums[0]));
 	printf("----------------------\n");
 	printf("sum: %d", sum);
