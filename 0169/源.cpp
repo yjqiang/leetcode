4bb5026238fdf7f8c73277931d93c1c538afa8b6
12345 <67890>
2019-12-30 22:36:55 +0800

    0169.0

---
+++ 0169/源.cpp
@@ -0,0 +1,90 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+
+
+typedef struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} TreeNode;
+
+#define QUEUE_SIZE 1000
+typedef struct {
+	struct TreeNode* val[QUEUE_SIZE];
+	int rear;
+	int front;
+}Queue;
+
+bool isEmpty(Queue *q) {
+	if (q->front == q->rear)
+		return true;
+	return false;
+}
+
+struct TreeNode* deQueue(Queue *q) {
+	if (isEmpty(q))
+		return NULL;
+	struct TreeNode *x = q->val[q->front];
+	q->front = (q->front + 1) % QUEUE_SIZE;
+	return x;
+}
+
+bool enQueue(Queue *q, struct TreeNode* x) {
+	if ((q->rear + 1) % QUEUE_SIZE == q->front)
+		return false;
+	q->val[q->rear] = x;
+	q->rear = (q->rear + 1) % QUEUE_SIZE;
+	return true;
+}
+
+int getQueueSize(Queue* q) {
+	if (isEmpty(q))
+		return NULL;
+	return (q->rear + QUEUE_SIZE - q->front) % QUEUE_SIZE;
+}
+
+void initQueue(Queue *q) {
+	q->front = 0;
+	q->rear = 0;
+}
+
+int max_y(int a, int b) {
+	return a > b ? a : b;
+}
+
+int min_y(int a, int b) {
+	return a < b ? a : b;
+}
+
+
+
+
+int majorityElement(int* nums, int numsSize) {
+	int cur = INT_MAX;
+	int count = 0;
+	int element;
+	for (int i = 0; i < numsSize; i++) {
+		element = nums[i];
+		if (element != cur) {
+			if (!count) {
+				count++;
+				cur = element;
+			}
+			else
+				count--;
+		}
+		else
+			count++;
+	}
+	return cur;
+}
+
+
+
+int main() {
+	int a[] = { 2,2,1,1,1,2,2 };
+	printf("%d\n", majorityElement(a, sizeof(a)/sizeof(a[0])));
+
+}
