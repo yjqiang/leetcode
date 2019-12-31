3200b647370fb3791f5b90b606af01664ee644a3
12345 <67890>
2019-12-31 09:58:30 +0800

    0190.0

---
+++ 0190/源.cpp
@@ -0,0 +1,78 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include<stdint.h>
+
+
+typedef struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} TreeNode;
+
+#define QUEUE_SIZE 1000
+typedef struct {
+	struct TreeNode* val[QUEUE_SIZE];
+	int rear;
+	int front;
+}Queue;
+
+bool isEmpty(Queue *q) {
+	if (q->front == q->rear)
+		return true;
+	return false;
+}
+
+struct TreeNode* deQueue(Queue *q) {
+	if (isEmpty(q))
+		return NULL;
+	struct TreeNode *x = q->val[q->front];
+	q->front = (q->front + 1) % QUEUE_SIZE;
+	return x;
+}
+
+bool enQueue(Queue *q, struct TreeNode* x) {
+	if ((q->rear + 1) % QUEUE_SIZE == q->front)
+		return false;
+	q->val[q->rear] = x;
+	q->rear = (q->rear + 1) % QUEUE_SIZE;
+	return true;
+}
+
+int getQueueSize(Queue* q) {
+	if (isEmpty(q))
+		return NULL;
+	return (q->rear + QUEUE_SIZE - q->front) % QUEUE_SIZE;
+}
+
+void initQueue(Queue *q) {
+	q->front = 0;
+	q->rear = 0;
+}
+
+int max_y(int a, int b) {
+	return a > b ? a : b;
+}
+
+int min_y(int a, int b) {
+	return a < b ? a : b;
+}
+
+
+uint32_t reverseBits(uint32_t n) {
+	uint32_t sum = 0;
+	// not zero
+	for (int i = 0; i < 32;i++) {
+		sum = sum * 2 + n % 2;
+		n = n / 2;
+	}
+	return sum;
+}
+
+
+int main() {
+	uint32_t n= 4294967293;
+	printf("%u\n", reverseBits(n));
+
+}
