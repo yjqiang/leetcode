36961f208218a4e56576c8b677efecea0e55da28
12345 <67890>
2019-12-31 10:24:08 +0800

    0191.0

---
+++ 0191/源.cpp
@@ -0,0 +1,77 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include<stdint.h>
+
+
+typedef struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} TreeNode;
+
+#define QUEUE_SIZE 1000
+typedef struct {
+	struct TreeNode* val[QUEUE_SIZE];
+	int rear;
+	int front;
+}Queue;
+
+bool isEmpty(Queue *q) {
+	if (q->front == q->rear)
+		return true;
+	return false;
+}
+
+struct TreeNode* deQueue(Queue *q) {
+	if (isEmpty(q))
+		return NULL;
+	struct TreeNode *x = q->val[q->front];
+	q->front = (q->front + 1) % QUEUE_SIZE;
+	return x;
+}
+
+bool enQueue(Queue *q, struct TreeNode* x) {
+	if ((q->rear + 1) % QUEUE_SIZE == q->front)
+		return false;
+	q->val[q->rear] = x;
+	q->rear = (q->rear + 1) % QUEUE_SIZE;
+	return true;
+}
+
+int getQueueSize(Queue* q) {
+	if (isEmpty(q))
+		return NULL;
+	return (q->rear + QUEUE_SIZE - q->front) % QUEUE_SIZE;
+}
+
+void initQueue(Queue *q) {
+	q->front = 0;
+	q->rear = 0;
+}
+
+int max_y(int a, int b) {
+	return a > b ? a : b;
+}
+
+int min_y(int a, int b) {
+	return a < b ? a : b;
+}
+
+
+int hammingWeight(uint32_t n) {
+	uint32_t sum = 0;
+	for (int i = 0; i < 32; i++) {
+		sum += n & 1;
+		n = n >>1;
+	}
+	return sum;
+}
+
+
+int main() {
+	uint32_t n= 0b11111111111111111111111111111101;
+	printf("%u\n", hammingWeight(n));
+
+}
