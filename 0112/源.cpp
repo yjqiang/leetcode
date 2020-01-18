7f4038f0bfe5218086637421d5d1a30d75616c94
12345 <67890>
2020-01-18 21:50:59 +0800

    0112.0

---
+++ 0112/源.cpp
@@ -0,0 +1,83 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include<stdint.h>
+#include <vector>
+#include <unordered_map> 
+
+# include "mystack.h"
+# include "myqueue.h"
+# include "mybinarytree.h"
+# include "my.h"
+using namespace std;
+
+#define null element_null
+
+char* int2bin(int a, char* buffer, int buf_size) {
+
+	for (int i = 0; i < 32; i++) {
+		*(buffer + buf_size - i - 1) = (a & 1) + '0';
+
+		a >>= 1;
+	}
+	for (int i = 0; i < 32; i++)
+		if (i % 4 == 3)
+			printf("%c,", buffer[i]);
+		else
+			printf("%c", buffer[i]);
+	printf("\n");
+	return buffer;
+}
+
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+
+void printListNodes(struct ListNode* head) {
+	for (struct ListNode* p = head; p != NULL; p = p->next)
+		printf("%d ", p->val);
+	printf("\n");
+}
+
+int result = 0;
+
+
+//int pathSum(struct TreeNode* root, int sum) {
+//	return 0;
+//}
+
+bool hasPathSum0(struct TreeNode* root, int sum) {
+	if (root) {
+		sum -= root->val;
+		if (!(root->left) && !(root->right) && (!sum))
+			return true;
+		if (hasPathSum0(root->left, sum) || hasPathSum0(root->right, sum))
+			return true;
+		return false;
+	}
+	return false;
+}
+
+bool hasPathSum(struct TreeNode* root, int sum) {
+	if (root) {
+		sum -= root->val;
+		if (!(root->left) && !(root->right) && (!sum))
+			return true;
+		if (hasPathSum(root->left, sum) || hasPathSum(root->right, sum))
+			return true;
+		return false;
+	}
+	return false;
+}
+
+int main() {
+	int a[] = { 1, 2 };
+	auto t = createTree(a, sizeof(a) / sizeof(a[0]));
+	printTree(t);
+	printf("\n");
+	printf("%d\n", hasPathSum(t, 1));
+}
