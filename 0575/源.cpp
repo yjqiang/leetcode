cab0ae1a3b0062bb8eb690e85c7a6998de2fc0ff
12345 <67890>
2020-01-19 22:52:57 +0800

    0575.0 hash

---
+++ 0575/源.cpp
@@ -0,0 +1,86 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include<stdint.h>
+#include <vector>
+#include <unordered_map> 
+
+# include "mystack.h"
+# include "myqueue.h"
+# include "mybinarytree.h"
+# include "my.h"
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+#define null element_null
+
+char* int2bin(int a, char* buffer, int buf_size) {
+
+	for (int i = 0; i < 32; i++) {
+		*(buffer + buf_size - i - 1) = (a & 1) + '0';
+
+		a >>= 1;
+	}
+	for (int i = 0; i < 32; i++)
+		if (i % 4 == 3)
+			printf("%c,", buffer[i]);
+		else
+			printf("%c", buffer[i]);
+	printf("\n");
+	return buffer;
+}
+
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+
+void printListNodes(struct ListNode* head) {
+	for (struct ListNode* p = head; p != NULL; p = p->next)
+		printf("%d ", p->val);
+	printf("\n");
+}
+
+void printUnorderedMap(unordered_map<int, int> m) {
+	for (auto i = m.begin(); i != m.end(); i++)
+		printf("map element: %d %d\n", i->first, i->second);
+	printf("............\n");
+}
+
+void printVector(vector<int> m) {
+	for (auto i = m.begin(); i != m.end(); i++)
+		printf("vector element: %d\n", *i);
+	printf("............\n");
+}
+
+void test(unordered_map<int, int> m) {
+	m[2222] = 33;
+	for (auto i = m.begin(); i != m.end(); i++)
+		printf("test map element: %d %d\n", i->first, i->second);
+}
+
+
+int distributeCandies(vector<int>& candies) {
+	unordered_set<int> m;
+	for (auto i = candies.begin(); i != candies.end(); i++) {
+		m.insert(*i);
+	}
+	return myMin((int)(candies.size() / 2), m.size());
+}
+
+
+
+
+int main() {
+	int a[] = { 1, null, 2, null, null, 2 };
+	vector<int> a0 = { 1,1,2,3 };
+	auto root = createTree(a, sizeof(a) / sizeof(a[0]));
+	// printTree(root);
+
+
+	printf("%d\n", distributeCandies(a0));
+}
