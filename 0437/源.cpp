9f69c1d7e6cae9bd58f7e31ec6828ff109c1d7dd
12345 <67890>
2020-01-19 16:05:18 +0800

    0437.1 前缀和

--- 0437/源.cpp
+++ 0437/源.cpp
@@ -43,38 +43,82 @@ void printListNodes(struct ListNode* head) {
 	printf("\n");
 }
 
-int hasPath(struct TreeNode* root, int sum) {
-	if (root) {
-		int result0 = 0;
-		int result1 = 0;
-		int result2 = 0;
+void printUnorderedMap(unordered_map<int, int> m) {
+	for (auto i = m.begin(); i != m.end(); i++)
+		printf("map element: %d %d\n", i->first, i->second);
+	printf("............\n");
+}
 
-		sum -= root->val;
-		if (!sum) {
-			result0 = 1;
-		}
-		result1 = hasPath(root->left, sum);
-		result2 = hasPath(root->right, sum);
-		return result0 + result1 + result2;
+void test(unordered_map<int, int> m) {
+	m[2222] = 33;
+	for (auto i = m.begin(); i != m.end(); i++)
+		printf("test map element: %d %d\n", i->first, i->second);
+}
+
+int subarraySum(vector<int>& nums, int k) {
+	unordered_map<int, int> cache;
+	int result = 0;
+	int sum = 0;
+	cache[0] = 1;
+	for (auto i = nums.begin(); i != nums.end(); i++) {
+		sum += *i;
+
+		if (cache.find(sum - k) != cache.end())
+			result += cache[sum - k];
+
+
+		/*if (cache.find(sum) != cache.end())
+			cache[sum] += 1;
+		else
+			cache[sum] = 1;*/
+		cache[sum] += 1;
+
+
 	}
-	return 0;
+	return result;
+}
+
+void help(TreeNode* root, int sum, unordered_map<int, int> &cache, int real_sum, int &result) {
+	if (root) {
+		real_sum += root->val;
+		if (cache.find(real_sum - sum) != cache.end())
+			result += cache[real_sum - sum];
+		cache[real_sum] += 1;
+		/*printf("%d:::", root->val);
+		printUnorderedMap(cache);*/
+		help(root->left, sum, cache, real_sum, result);
+		help(root->right, sum, cache, real_sum, result);
+		cache[real_sum] -= 1;
+	}
+}
+
+int pathSum(TreeNode* root, int sum) {
+	unordered_map<int, int> cache;
+	cache[0] = 1;
+	int result = 0;
+	help(root, sum, cache, 0, result);
+	return result;
 }
 
 
-int pathSum(struct TreeNode* root, int sum) {
-	if (root) {
-		int result0 = hasPath(root, sum);
-		int result1 = pathSum(root->left, sum);
-		int result2 = pathSum(root->right, sum);
-		return result0 + result1 + result2;
-	}
-	return 0;
-}
 
 int main() {
-	int a[] = { 5,4,8,11,null,13,4,7,2,null,null,5,1};
+	int a[] = { 10,5,-3,3,2,null,11,3,-2,null,1 };
+	vector<int> a0 = { 1,1, 1 };
 	auto root = createTree(a, sizeof(a) / sizeof(a[0]));
 	printTree(root);
 	printf("\n");
-	printf("%d\n", pathSum(root, 22));
+	unordered_map<int, int> umap;
+	printf("%d|\n", umap[11]);
+	test(umap);
+	test(umap);
+	test(umap);
+	for (auto i = umap.begin(); i != umap.end(); i++)
+		printf("map element: %d %d\n", i->first, i->second);
+
+	
+
+	printf("------------------\n");
+	printf("pathSum %d\n", pathSum(root, 8));
+	printf("%d\n", subarraySum(a0, sizeof(a) / sizeof(a[0])));
 }
