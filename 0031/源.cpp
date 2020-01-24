3691ed1b87726f0c4c1d75a52b5092774388ab53
12345 <67890>
2020-01-24 11:11:27 +0800

    0031.0 非递归（BEST）

--- 0031/源.cpp
+++ 0031/源.cpp
@@ -57,52 +57,35 @@ void print2dVector(vector<vector<int>> m) {
 	printf("----------------------\n");
 }
 
-bool help(int* nums, int numsSize, int left){
-	if (left < numsSize - 1){
-		if (help(nums, numsSize, left + 1))
-			return true;
-		if (nums[left] < nums[left + 1]){
-			int bigger_index = left + 1;
-			int target = nums[left];
-			for (; bigger_index < numsSize && nums[bigger_index] > target; ++bigger_index);
-			--bigger_index;
 
-			int tmp = target;
-			nums[left] = nums[bigger_index];
-			nums[bigger_index] = tmp;
-
-			// reverse
-			for (int i = left + 1, j = numsSize - 1; i < j; ++i, --j)
-			{
-				tmp = nums[i];
-				nums[i] = nums[j];
-				nums[j] = tmp;
-			}
-			return true;
-		}
-		
-		
-	}
-	return false;
-}
 
 void nextPermutation(int* nums, int numsSize) {
-	if (!help(nums, numsSize, 0)){
-		int tmp;
-		// reverse
-		for (int i = 0, j = numsSize - 1; i < j; ++i, --j)
-		{
-			tmp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = tmp;
-		}
+	int p, tmp;
+	for (p = numsSize - 2; p >= 0 && nums[p] >= nums[p + 1]; --p);
+	if (p != -1){
+		int bigger_index;
+		int target = nums[p];
+		for (bigger_index = p + 1; bigger_index < numsSize && nums[bigger_index] > target; ++bigger_index);
+		--bigger_index;
+
+		int tmp = target;
+		nums[p] = nums[bigger_index];
+		nums[bigger_index] = tmp;
+
+	}
+	
+	// reverse
+	for (int i = p + 1, j = numsSize - 1; i < j; ++i, --j){
+		tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
 	}
 }
 
 
 
 int main() {
-	int a[] = { 1, 2, 3 };
+	int a[] = { 1, 5, 1 };
 	int size = sizeof(a) / sizeof(a[0]);
 	nextPermutation(a, size);
 	for (int i = 0; i < size; ++i)
