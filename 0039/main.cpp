c12bdccd97f4f77f6d13305aef31351c46cb948c
12345 <67890>
2020-02-11 12:24:22 +0800

    0039.1 感觉类似一个先序遍历

--- 0039/main.cpp
+++ 0039/main.cpp
@@ -57,42 +57,30 @@ void print2dVector(vector<vector<int>> m) {
     printf("----------------------\n");
 }
 
-vector<int> tmp;
-
-void func(vector<int>& candidates, int target, int end, int sum, vector<vector<int>> &result){
-    if (end < 0)
-        return;
-    int end_element = candidates[end];
-    int tmp_sum = end_element + sum;
-    //printf("sum=%d tmp_sum=%d end=%d \n", sum, tmp_sum, end);
-    if (target == tmp_sum){
-        tmp.push_back(end_element);
+void func(vector<int>& candidates, int target, int begin, vector<vector<int>> &result, vector<int> &tmp){
+    if (!target){
         result.push_back(tmp);
-
-        tmp.pop_back();
-        func(candidates, target, end - 1, sum, result);
-
+        return;
     }
-    else if (target < tmp_sum){
-        func(candidates, target, end - 1, sum, result);
-    }
-    else{
-        tmp.push_back(end_element);
-        func(candidates, target, end, tmp_sum, result);
+    for (int i = begin; i < candidates.size() && candidates[i] <= target; ++i){
+        tmp.push_back(candidates[i]);
+        func(candidates, target-candidates[i], i, result, tmp);
         tmp.pop_back();
-        func(candidates, target, end - 1, sum, result);
+
     }
 }
 
 
 vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
     vector<vector<int>> result;
-    func(candidates, target, (int)candidates.size()-1, 0, result);
+    vector<int> tmp;
+    sort(candidates.begin(), candidates.end());
+    func(candidates, target, 0, result, tmp);
     return result;
 }
 
 int main() {
-    vector<int> a = {2,3,6,7};
-    print2dVector(combinationSum(a, 7));
+    vector<int> a = {8,7,4,3};
+    print2dVector(combinationSum(a, 11));
     return 0;
 }
