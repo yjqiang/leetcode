448e625a4ded64ca09819fadc80c6f3cfcfe3f25
yjqiang <yjqiang1@gmail.com>
2020-02-22 10:48:35 +0800

    0062.11 动态规划优化空间复杂度

---
+++ 0062/main_1b.cpp
@@ -0,0 +1,85 @@
+# include <cstdio>
+# include <cstring>
+# include <cstdlib>
+# include <climits>
+# include <cstdint>
+# include <vector>
+# include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <unordered_set>
+#include <bitset>
+#include <iostream>
+
+# include "mystack.h"
+# include "myqueue.h"
+# include "mybinarytree.h"
+# include "mylinkedlist.h"
+# include "my.h"
+#include <algorithm>
+
+using namespace std;
+
+#define null element_null
+
+char* int2bin(int a, char* buffer, int buf_size) {
+
+    for (int i = 0; i < 32; i++) {
+        *(buffer + buf_size - i - 1) = (a & 1) + '0';
+
+        a >>= 1;
+    }
+    for (int i = 0; i < 32; i++)
+        if (i % 4 == 3)
+            printf("%c,", buffer[i]);
+        else
+            printf("%c", buffer[i]);
+    printf("\n");
+    return buffer;
+}
+
+
+
+void printUnorderedMap(unordered_map<int, int> m) {
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printf("map element: %d %d\n", i->first, i->second);
+    printf("............\n");
+}
+
+void printVector(vector<int> m) {
+    printf("vector 1d: ");
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printf("%4d ", *i);
+    printf("\n");
+}
+
+void print2dVector(vector<vector<int>> m) {
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printVector(*i);
+    printf("----------------------\n");
+}
+
+static bool cmp (vector<int> &a, vector<int> &b){
+    return a[0] < b[0];
+}
+
+int cache[1001] = {0};
+
+int uniquePaths(int m, int n) {
+    if (m==1 || n==1)
+        return 1;
+
+    for (int i = 1; i<= n; ++i)
+        for(int j=1; j<= m; ++j){
+            if (i==1||j==1)
+                cache[j] = 1;
+            else
+                cache[j] = cache[j] + cache[j-1];
+        }
+    return cache[m];
+}
+
+int main() {
+    cout << uniquePaths(10, 10);
+    return 0;
+}
