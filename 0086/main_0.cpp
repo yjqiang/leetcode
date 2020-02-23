02dfe06b592db1c51b6d7d345b34bbfdbd349598
yjqiang <yjqiang1@gmail.com>
2020-02-23 16:02:06 +0800

    0086.0 尾插+删除

---
+++ 0086/main_0.cpp
@@ -0,0 +1,99 @@
+# include <cstdio>
+# include <cstring>
+# include <cstdlib>
+# include <climits>
+# include <cstdint>
+# include <vector>
+# include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <unordered_set>
+#include <bitset>
+#include <iostream>
+
+# include "mystack.h"
+# include "myqueue.h"
+# include "mybinarytree.h"
+# include "mylinkedlist.h"
+# include "my.h"
+#include <algorithm>
+
+using namespace std;
+
+#define null element_null
+
+char* int2bin(int a, char* buffer, int buf_size) {
+
+    for (int i = 0; i < 32; i++) {
+        *(buffer + buf_size - i - 1) = (a & 1) + '0';
+
+        a >>= 1;
+    }
+    for (int i = 0; i < 32; i++)
+        if (i % 4 == 3)
+            printf("%c,", buffer[i]);
+        else
+            printf("%c", buffer[i]);
+    printf("\n");
+    return buffer;
+}
+
+
+
+void printUnorderedMap(unordered_map<int, int> m) {
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printf("map element: %d %d\n", i->first, i->second);
+    printf("............\n");
+}
+
+void printVector(vector<int> m) {
+    printf("vector 1d: ");
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printf("%4d ", *i);
+    printf("\n");
+}
+
+void print2dVector(vector<vector<int>> m) {
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printVector(*i);
+    printf("----------------------\n");
+}
+
+static bool cmp (vector<int> &a, vector<int> &b){
+    return a[0] < b[0];
+}
+
+struct ListNode* partition(struct ListNode* head, int x){
+    struct ListNode* less_list = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* fake = (struct ListNode*)malloc(sizeof(struct ListNode));
+    fake->next = head;
+    head = fake;
+
+
+    struct ListNode* p, *pre, *less_list_p;
+
+    for (p=head->next, pre=head, less_list_p=less_list; p;){
+        if (p->val < x){
+            less_list_p->next = p;
+            pre->next = p->next;
+            p=p->next;
+            less_list_p = less_list_p->next;
+        }
+        else {
+            pre = p;
+            p = p->next;
+        }
+
+    }
+    less_list_p->next = fake->next;
+    return less_list->next;
+
+}
+
+int main() {
+    int a[] = {1, 4, 3, 2, 5, 2};
+    struct ListNode* head = myLinkedListCreate(a, sizeof(a)/ sizeof(a[0]));
+    myLinkedListPrint(partition(head, 3));
+
+    return 0;
+}
