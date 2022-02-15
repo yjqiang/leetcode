3c38e67f9403b4b5c193a671cbace5d32f8b5dc0
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-02-15 12:59:52 +0800

    判定字母数字用 cpp 的实现

--- 0125/源.cpp
+++ 0125/源.cpp
@@ -1,92 +1,53 @@
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
-# include <limits.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
 
 
-typedef struct TreeNode {
-	int val;
-	struct TreeNode* left;
-	struct TreeNode* right;
-} TreeNode;
-
-#define QUEUE_SIZE 1000
-typedef struct {
-	struct TreeNode* val[QUEUE_SIZE];
-	int rear;
-	int front;
-}Queue;
-
-bool isEmpty(Queue *q) {
-	if (q->front == q->rear)
-		return true;
-	return false;
+template<typename T>
+void printVector(const T& t) {
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
 }
 
-struct TreeNode* deQueue(Queue *q) {
-	if (isEmpty(q))
-		return NULL;
-	struct TreeNode *x = q->val[q->front];
-	q->front = (q->front + 1) % QUEUE_SIZE;
-	return x;
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
 }
 
-bool enQueue(Queue *q, struct TreeNode* x) {
-	if ((q->rear + 1) % QUEUE_SIZE == q->front)
-		return false;
-	q->val[q->rear] = x;
-	q->rear = (q->rear + 1) % QUEUE_SIZE;
-	return true;
-}
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left, right;
+        char left_char, right_char;
+        for (left = 0, right = (int)s.size()-1; left < right;){
+            left_char = (char)tolower(s[left]);
+            right_char = (char)tolower(s[right]);
 
-int getQueueSize(Queue* q) {
-	if (isEmpty(q))
-		return NULL;
-	return (q->rear + QUEUE_SIZE - q->front) % QUEUE_SIZE;
-}
+            if (!isalnum(left_char))
+                ++left;
+            else if (!isalnum(right_char))
+                --right;
+            else {  //  isalnum(left_char) && isalnum(right_char)
+                if (left_char == right_char) {
+                    ++left;
+                    --right;
+                }
+                else
+                    return false;
+            }
 
-void initQueue(Queue *q) {
-	q->front = 0;
-	q->rear = 0;
-}
-
-int max_y(int a, int b) {
-	return a > b ? a : b;
-}
-
-int min_y(int a, int b) {
-	return a < b ? a : b;
-}
-
-bool isPalindrome(char* s) {
-	int length = strlen(s);
-	char tmp0, tmp1;
-	for (int i = 0, j = length - 1; i < j;) {
-		if (!(s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9')) {
-			i++;
-			continue;
-		}
-		if (!(s[j] >= 'A' && s[j] <= 'Z' || s[j] >= 'a' && s[j] <= 'z' || s[j] >= '0' && s[j] <= '9')) {
-			j--;
-			continue;
-		}
-
-		tmp0 = (s[i] >= 'A' && s[i] <= 'Z') ? s[i] + 32 : s[i];
-		tmp1 = (s[j] >= 'A' && s[j] <= 'Z') ? s[j] + 32 : s[j];
-		printf("s[%d]=%c%c  s[%d]=%c%c\n", i, s[i], tmp0, j, s[j], tmp1);
-		if (tmp0 != tmp1)
-			return false;
-		i++;
-		j--;
-	}
-	return true;
-}
-
-
+        }
+        return true;
+    }
+};
 
 int main() {
-	char a[] = "pP";
-	printf("BEST: %d", isPalindrome(a));
+    string s = "A man, a plan, a canal: Panama";
+    bool result = Solution().isPalindrome(s);
+    cout << result << endl;
 
-
+    return 0;
 }
