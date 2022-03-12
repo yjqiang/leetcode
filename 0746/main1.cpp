20a46cb6cbb0f48f9806c665e82b6de57b85dbfe
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-03-12 12:16:16 +0800

    空间复杂度O（1）

---
+++ 0746/main1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <cstdlib>
+
+using namespace std;
+
+
+#define null (-99)
+
+template<typename T>
+void printVector(const T& t) {
+    printf("[");
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    printf("], ");
+}
+
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* createListNode(vector<int> &a) {
+    ListNode *p;
+    ListNode *head, *tail;
+
+    head = new ListNode();
+    head->val = a[0];
+    head->next = nullptr;
+
+    int i;
+    for (i = 1, tail = head; i < a.size(); ++i) {
+        p = new ListNode();
+        p->val = a[i];
+        tail->next = p;
+        tail = p;
+    }
+    tail->next = nullptr;
+    return head;
+}
+
+
+void printListNodes(ListNode* head) {
+    for (ListNode* p = head; p != nullptr; p = p->next)
+        printf("%d -> ", p->val);
+    printf("NULL\n");
+}
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = (int)cost.size();
+        int pre_pre_dp, pre_dp, cur_dp;
+        int i;
+        for (i = 2, pre_pre_dp = 0, pre_dp = 0; i <= n;){
+            cur_dp = min(pre_pre_dp+cost[i-2], pre_dp+cost[i-1]);
+
+            ++i;
+            pre_pre_dp = pre_dp;
+            pre_dp = cur_dp;
+        }
+        return cur_dp;
+    }
+};
+
+int main(){
+    vector<int> cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+    int answer = Solution().minCostClimbingStairs(cost);
+    cout << answer << endl;
+}
