3ce650a7ff40d48c6e26d426503ed6d3d783c772
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-03-14 09:58:46 +0800

    动态规划

---
+++ 0926/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <cstdlib>
+
+using namespace std;
+
+
+#define null (-99)
+
+template<typename T>
+void printVector(const T& t) {
+    printf("[");
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    printf("], ");
+}
+
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* createListNode(vector<int> &a) {
+    ListNode *p;
+    ListNode *head, *tail;
+
+    head = new ListNode();
+    head->val = a[0];
+    head->next = nullptr;
+
+    int i;
+    for (i = 1, tail = head; i < a.size(); ++i) {
+        p = new ListNode();
+        p->val = a[i];
+        tail->next = p;
+        tail = p;
+    }
+    tail->next = nullptr;
+    return head;
+}
+
+
+void printListNodes(ListNode* head) {
+    for (ListNode* p = head; p != nullptr; p = p->next)
+        printf("%d -> ", p->val);
+    printf("NULL\n");
+}
+
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int n = (int)s.size();
+        // dp[i][1] and dp[i][0]
+        int one, zero;
+        zero = s[0] - '0';
+        one = '1' - s[0];
+
+        int i;
+        for(i = 1; i < n; ++i){
+            one = min(zero, one) + ('1' - s[i]);
+            zero = zero + (s[i] - '0');
+        }
+
+        return min(zero, one);
+    }
+};
+
+int main(){
+    string s = "010110";
+    int answer = Solution().minFlipsMonoIncr(s);
+    cout << answer << endl;
+}
