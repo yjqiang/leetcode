043b165a5afb7fc5c669df4d24ef955ea50cc7e3
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-06-13 11:08:49 +0800

    update

---
+++ 0031/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stdlib.h>
+
+using namespace std;
+
+#define null (-99)
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+template<typename T>
+void printVector(const T& t) {
+    cout << "[";
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
+}
+
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+
+ListNode* stringToListNode(vector<int> &list) {
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
+        printf("%d -> ", p->val);
+    printf("null\n");
+}
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i, j, k;
+        for (i = (int)nums.size() - 2, j = i + 1; i >= 0 && nums[i] >= nums[j]; --i, --j);
+        if (i >= 0){
+            for (k = (int)nums.size() - 1; nums[k] <= nums[i]; --k);
+            swap(nums[i], nums[k]);
+        }
+
+        int left, right;
+        for (left = i + 1, right = (int)nums.size() - 1; left < right; ++left, --right)
+            swap(nums[left], nums[right]);
+    }
+};
+
+int main(){
+    vector<int> nums = {2,3,1};
+    Solution().nextPermutation(nums);
+    printVector(nums);
+}
