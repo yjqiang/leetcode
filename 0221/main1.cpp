9bcd6ce35f5cb4c1845a52d04bfdff59ed9b3de1
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-06-25 22:57:31 +0800

    用 0085 老方法试试

---
+++ 0221/main1.cpp
@@ -0,0 +1,222 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stdlib.h>
+
+using namespace std;
+
+#define null (-99)
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+template<typename T>
+void printVector(const T& t) {
+    cout << "[";
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
+}
+
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* createTree(vector<int>& inputs){
+    queue<TreeNode*> my_queue;
+    TreeNode* root = new TreeNode(inputs[0]);
+    my_queue.push(root);
+
+    TreeNode *node;
+    int i = 0;
+    while (!my_queue.empty()) {
+        node = my_queue.front();
+        my_queue.pop();
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+
+        if (inputs[i] != null) {
+            node->left = new TreeNode(inputs[i]);
+            my_queue.push(node->left);
+        }
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+        if (inputs[i] != null) {
+            node->right = new TreeNode(inputs[i]);
+            my_queue.push(node->right);
+        }
+    }
+    return root;
+
+}
+
+struct Trunk
+{
+    Trunk *prev;
+    string str;
+
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
+
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr) {
+        return;
+    }
+
+    showTrunks(p->prev);
+    cout << p->str;
+}
+
+void printTree(TreeNode* root, Trunk *prev, bool isLeft)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(root->right, trunk, true);
+
+    if (!prev) {
+        trunk->str = "---";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "/---";
+        prev_str = "   |";
+    }
+    else {
+        trunk->str = "\\---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << " " << root->val << endl;
+
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "   |";
+
+    printTree(root->left, trunk, false);
+}
+
+ListNode* stringToListNode(vector<int> &list) {
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
+        printf("%d -> ", p->val);
+    printf("null\n");
+}
+
+class MyNode{
+    public:
+
+    vector<MyNode*> nexts;
+    bool is_one_word_end;
+
+    MyNode(){
+        nexts = vector<MyNode*>(26, nullptr);
+        is_one_word_end = false;
+    }
+};
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = (int)matrix.size(), n = (int)matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n)); vector<int> heights(n, 0);
+        int i, j, answer = 0;
+        // update heights
+        for (j = 0; j < n; ++j)
+            if (matrix[0][j] == '1')
+                ++heights[j];
+            else
+                heights[j] = 0;
+        for (j = 0; j < n; ++j) {
+            dp[0][j] = matrix[0][j] == '1' ? 1 : 0;
+            answer = max(dp[0][j], answer);
+        }
+        // 和 heights 类似作用（只是反转了一下）的值，指以当前行以 matrix[i][j] 为右边界，最长连续 1（只看这一行）的左边界
+        int lb;
+        for (i = 1; i < m; ++i){
+            // update heights
+            for (j = 0; j < n; ++j)
+                if (matrix[i][j] == '1')
+                    ++heights[j];
+                else
+                    heights[j] = 0;
+
+            lb = matrix[i][0] == '1'? 0: 1;
+
+            dp[i][0] = matrix[i][0] == '1'? 1: 0;
+            answer = max(dp[i][0], answer);
+
+            for (j = 1; j < n; ++j) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = min(dp[i - 1][j - 1] + 1, min(heights[j], j - lb + 1));
+                    answer = max(dp[i][j], answer);
+                }
+                else {
+                    lb = j + 1;
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return answer*answer;
+    }
+};
+
+int main() {
+    vector<vector<char>> matrix = {
+            {'0','0','0','0','0'},
+            {'0','0','0','0','0'},
+            {'0','0','0','0','1'},
+            {'0','0','0','0','0'}
+    };
+    int answer = Solution().maximalSquare(matrix);
+    cout << answer << endl;
+}
