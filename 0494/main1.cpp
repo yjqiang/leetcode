603f738826c8e8d45f558fba13e0166d2a86dfeb
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-07-08 02:21:47 +0800

    Update main1.cpp

--- 0494/main1.cpp
+++ 0494/main1.cpp
@@ -3,26 +3,14 @@
 #include <string>
 #include <algorithm>
 #include <unordered_map>
-#include <map>
+#include <unordered_set>
 #include <queue>
-#include <cstdlib>
+#include <stdlib.h>
 
 using namespace std;
 
-
 #define null (-99)
 
-template<typename T>
-void printVector(const T& t) {
-    printf("[");
-    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
-    printf("], ");
-}
-
-template<typename T>
-void printVectorInVector(const T& t) {
-    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
-}
 
 struct ListNode {
     int val;
@@ -32,68 +20,169 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode* createListNode(vector<int> &a) {
-    ListNode *p;
-    ListNode *head, *tail;
-
-    head = new ListNode();
-    head->val = a[0];
-    head->next = nullptr;
-
-    int i;
-    for (i = 1, tail = head; i < a.size(); ++i) {
-        p = new ListNode();
-        p->val = a[i];
-        tail->next = p;
-        tail = p;
-    }
-    tail->next = nullptr;
-    return head;
+template<typename T>
+void printVector(const T& t) {
+    cout << "[";
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
 }
 
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
 
-void printListNodes(ListNode* head) {
-    for (ListNode* p = head; p != nullptr; p = p->next)
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* createTree(vector<int>& inputs){
+    queue<TreeNode*> my_queue;
+    TreeNode* root = new TreeNode(inputs[0]);
+    my_queue.push(root);
+
+    TreeNode *node;
+    int i = 0;
+    while (!my_queue.empty()) {
+        node = my_queue.front();
+        my_queue.pop();
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+
+        if (inputs[i] != null) {
+            node->left = new TreeNode(inputs[i]);
+            my_queue.push(node->left);
+        }
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+        if (inputs[i] != null) {
+            node->right = new TreeNode(inputs[i]);
+            my_queue.push(node->right);
+        }
+    }
+    return root;
+
+}
+
+struct Trunk
+{
+    Trunk *prev;
+    string str;
+
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
+
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr) {
+        return;
+    }
+
+    showTrunks(p->prev);
+    cout << p->str;
+}
+
+void printTree(TreeNode* root, Trunk *prev, bool isLeft)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(root->right, trunk, true);
+
+    if (!prev) {
+        trunk->str = "---";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "/---";
+        prev_str = "   |";
+    }
+    else {
+        trunk->str = "\\---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << " " << root->val << endl;
+
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "   |";
+
+    printTree(root->left, trunk, false);
+}
+
+ListNode* stringToListNode(vector<int> &list) {
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
         printf("%d -> ", p->val);
-    printf("NULL\n");
+    printf("null\n");
 }
 
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum = 0;
-        int i, j;
+        int i;
         for (i = 0; i < nums.size(); ++i)
             sum += nums[i];
 
-        if (target < -sum || target > sum || (sum + target) % 2 == 1)
+        if ((target + sum ) %2 == 1 || target > sum || target < -sum)
             return 0;
 
-        int V = (sum + target) / 2;
-        // dp[0][0...V] = 0
-        vector<int> dp(V + 1, 0);
+        int V = (target + sum) / 2;
+
+        int j;
+        vector<int> dp(V+1);
         dp[0] = 1;
+        for (i = 1; i <= nums.size(); ++i)
+            for (j = V; j >= 0; --j)
+                if (j - nums[i - 1] >= 0)
+                    // dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j]
+                    dp[j] += dp[j - nums[i - 1]];
 
-        if (nums[0] <= V)
-            dp[nums[0]] += 1;
-
-        for (i = 1; i < nums.size(); ++i)
-            for (j = V; j >= 0; --j){
-                if (j-nums[i] < 0)
-                    // dp[i][v] = dp[i-1][v]
-                    dp[j] = dp[j];
-                else
-                    // dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i]]
-                    dp[j] = dp[j] + dp[j-nums[i]];
-            }
         return dp[V];
     }
 };
 
+int main() {
+    vector<int> nums = {1,1,1,1,1};
+    int target = 3;
 
-int main(){
-    vector<int> nums = {1000};
-    int target = -1000;
-    int answer = Solution().findTargetSumWays(nums, target);
-    cout << answer << endl;
+    int ans = Solution().findTargetSumWays(nums, target);
+
+    cout << ans << endl;
 }
