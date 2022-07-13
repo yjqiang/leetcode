59db796701c58ae61c2a3f8e425a9c6a8e22f553
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-07-14 03:18:06 +0800

    优雅

--- 0304/main.cpp
+++ 0304/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <string>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stdlib.h>
 
 using namespace std;
 
+#define null (-99)
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 template<typename T>
 void printVector(const T& t) {
+    cout << "[";
     std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
 }
 
 template<typename T>
@@ -17,64 +32,155 @@ void printVectorInVector(const T& t) {
     std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
 }
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* createTree(vector<int>& inputs){
+    queue<TreeNode*> my_queue;
+    TreeNode* root = new TreeNode(inputs[0]);
+    my_queue.push(root);
+
+    TreeNode *node;
+    int i = 0;
+    while (!my_queue.empty()) {
+        node = my_queue.front();
+        my_queue.pop();
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+
+        if (inputs[i] != null) {
+            node->left = new TreeNode(inputs[i]);
+            my_queue.push(node->left);
+        }
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+        if (inputs[i] != null) {
+            node->right = new TreeNode(inputs[i]);
+            my_queue.push(node->right);
+        }
+    }
+    return root;
+
+}
+
+struct Trunk
+{
+    Trunk *prev;
+    string str;
+
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
+
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr) {
+        return;
+    }
+
+    showTrunks(p->prev);
+    cout << p->str;
+}
+
+void printTree(TreeNode* root, Trunk *prev, bool isLeft)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(root->right, trunk, true);
+
+    if (!prev) {
+        trunk->str = "---";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "/---";
+        prev_str = "   |";
+    }
+    else {
+        trunk->str = "\\---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << " " << root->val << endl;
+
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "   |";
+
+    printTree(root->left, trunk, false);
+}
+
+ListNode* stringToListNode(vector<int> &list) {
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
+        printf("%d -> ", p->val);
+    printf("null\n");
+}
 
 class NumMatrix {
 public:
-    vector<vector<int>> sum;
+    vector<vector<int>> matrix_sum;
 
     NumMatrix(vector<vector<int>>& matrix) {
         int m = (int)matrix.size(), n = (int)matrix[0].size();
-        // sum[m][n]
-        sum.resize(m, vector<int>(n));
-
-        int x, y;
-        int cur_row_sum;  // sum(matrix[x][0, 1, ..., y])
-
-        cur_row_sum = 0;
-        for (y = 0; y < n; ++y){
-            cur_row_sum += matrix[0][y];
-            sum[0][y] = cur_row_sum;
-        }
-
-        for (x = 1; x < m; ++x){
+        // matrix_sum[i][j] 从 [0,0] 到 [i-1,j-1] 范围的矩形的元素和
+        matrix_sum = vector<vector<int>> (m+1, vector<int>(n+1, 0));
+        int i, j, cur_row_sum;
+        for (i = 1; i <= m; ++i) {
+            // 本行的累加结果
             cur_row_sum = 0;
-            for (y = 0; y < n; ++y){
-                cur_row_sum += matrix[x][y];
-                sum[x][y] = sum[x - 1][y] + cur_row_sum;
+
+            for (j = 1; j <= n; ++j) {
+                cur_row_sum += matrix[i-1][j-1];
+                matrix_sum[i][j] = matrix_sum[i-1][j] + cur_row_sum;
             }
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int result = 0;
-        result += sum[row2][col2];
-        if (row1 > 0)
-            result -= sum[row1 - 1][col2];
-        if (col1 > 0)
-            result -= sum[row2][col1 - 1];
-        if (row1 > 0 && col1 > 0)
-            result += sum[row1 - 1][col1 - 1];
-        return result;
+        return matrix_sum[row2+1][col2+1] - matrix_sum[row1][col2+1] - matrix_sum[row2+1][col1] + matrix_sum[row1][col1];
     }
 };
 
 int main() {
-    vector<vector<int>> matrix = {
-            {3,0,1,4,2},
-            {5,6,3,2,1},
-            {1,2,0,1,5},
-            {4,1,0,1,7},
-            {1,0,3,0,5}};
+    vector<vector<int>>matrix = {{{3,0,1,4,2},{5,6,3,2,1},{1,2,0,1,5},{4,1,0,1,7},{1,0,3,0,5}}};
+    NumMatrix* obj = new NumMatrix(matrix);
+    int ans = obj->sumRegion(2,1,4,3);
 
-    NumMatrix* numMatrix = new NumMatrix(matrix);
-
-    int result;
-    result = numMatrix -> sumRegion(2,1,4,3);
-    cout << result << endl;
-    result = numMatrix -> sumRegion(1,1,2,2);
-    cout << result << endl;
-    result = numMatrix -> sumRegion(1,2,2,4);
-    cout << result << endl;
-
-    return 0;
+    cout << ans << endl;
 }
