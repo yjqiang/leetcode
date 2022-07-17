09b451cb1e2ecb97b88a03f81660c1f8ddd8fba7
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-07-17 22:37:40 +0800

    Update main.cpp

--- 0234/main.cpp
+++ 0234/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <string>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stdlib.h>
 
 using namespace std;
 
+#define null (-99)
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 template<typename T>
 void printVector(const T& t) {
+    cout << "[";
     std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
 }
 
 template<typename T>
@@ -17,21 +32,103 @@ void printVectorInVector(const T& t) {
     std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
 }
 
-struct ListNode {
+struct TreeNode {
     int val;
-    struct ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+TreeNode* createTree(vector<int>& inputs){
+    queue<TreeNode*> my_queue;
+    TreeNode* root = new TreeNode(inputs[0]);
+    my_queue.push(root);
 
-void printListNodes(struct ListNode* head) {
-    int i;
-    struct ListNode* p;
-    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
-        printf("%d -> ", p->val);
-    printf("null\n");
+    TreeNode *node;
+    int i = 0;
+    while (!my_queue.empty()) {
+        node = my_queue.front();
+        my_queue.pop();
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+
+        if (inputs[i] != null) {
+            node->left = new TreeNode(inputs[i]);
+            my_queue.push(node->left);
+        }
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+        if (inputs[i] != null) {
+            node->right = new TreeNode(inputs[i]);
+            my_queue.push(node->right);
+        }
+    }
+    return root;
+
+}
+
+struct Trunk
+{
+    Trunk *prev;
+    string str;
+
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
+
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr) {
+        return;
+    }
+
+    showTrunks(p->prev);
+    cout << p->str;
+}
+
+void printTree(TreeNode* root, Trunk *prev, bool isLeft)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(root->right, trunk, true);
+
+    if (!prev) {
+        trunk->str = "---";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "/---";
+        prev_str = "   |";
+    }
+    else {
+        trunk->str = "\\---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << " " << root->val << endl;
+
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "   |";
+
+    printTree(root->left, trunk, false);
 }
 
 ListNode* stringToListNode(vector<int> &list) {
@@ -47,69 +144,61 @@ ListNode* stringToListNode(vector<int> &list) {
     return ptr;
 }
 
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
+        printf("%d -> ", p->val);
+    printf("null\n");
+}
 
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode* mid_pre = findMidLeft(head);
-        ListNode* mid = mid_pre->next;
-        printListNodes(mid);
-        // len(second_part) == len(first_part) OR len(second_part) == len(first_part) + 1
-        ListNode* second_part = reverseList(mid);
-        mid_pre->next = nullptr;
+        if (head == nullptr || head->next == nullptr)
+            return true;
+        ListNode *fast, *slow, *pre_slow;
+        // 此时链表长度至少为 2
+        // slow 指向的那一半比左侧长度来说，或者长度+1（总长度为奇数），或者长度相同（总长度为偶数）
+        // pre_slow 指向 slow 前面的元素
+        for (fast=slow=head; fast != nullptr && fast->next != nullptr; fast = fast->next->next, slow = slow->next)
+            pre_slow = slow;
 
-        // compare
-        ListNode* p1, *p2;
-        bool result = true;
-        for (p1 = head, p2 = second_part; p2 != nullptr && p1 != nullptr; p1 = p1->next, p2 = p2->next){
-            if (p1->val != p2->val){
-                result = false;
-                break;
-            }
-        }
+        pre_slow->next = nullptr;
 
-        // repair
-        reverseList(second_part);
-        mid_pre->next = mid;
-        return result;
+        ListNode *reversed = reverseNodeList(slow);
 
+        bool answer = isSame(head, reversed);
+
+        // 还原
+        pre_slow->next = reverseNodeList(reversed);
+
+        return answer;
     }
 
-    ListNode* findMidLeft(ListNode* head) {
-        ListNode* fast, *slow;
-        fast = head;
-        slow = head;
-        // mid->pre
-        ListNode* mid_pre = head;
-        while(true){
-            if (fast == nullptr || fast->next == nullptr)
-                break;
-            mid_pre = slow;
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        return mid_pre;
+    bool isSame(ListNode *l, ListNode *r){
+        ListNode *p, *q;
+        for (p = l, q = r; p!= nullptr && q!= nullptr; p = p->next, q = q->next)
+            if (p->val != q->val) return false;
+        return true;
     }
 
-    ListNode* reverseList(ListNode* head) {
-        if (head == nullptr)
-            return head;
-        ListNode* p, *tmp;
-        for (p = head->next, head->next = nullptr; p != nullptr;){
+    ListNode* reverseNodeList(ListNode* head) {
+        ListNode *answer = nullptr;
+        ListNode *p, *tmp;
+        for (p = head; p != nullptr;){
             tmp = p->next;
-            p->next = head;
-            head = p;
+            p->next = answer;
+            answer = p;
 
             p = tmp;
         }
-
-        return head;
+        return answer;
     }
-
 };
 
 int main() {
-    vector<int> list = {1};
+    vector<int> list = {1,2,1};
     ListNode* head = stringToListNode(list);
 
     int result = Solution().isPalindrome(head);
