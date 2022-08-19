f43f14c9e0b9d086cc87a1fa662dc65866eabba8
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-08-19 16:35:53 +0800

    Update main.cpp

--- 0132/main.cpp
+++ 0132/main.cpp
@@ -3,26 +3,14 @@
 #include <string>
 #include <algorithm>
 #include <unordered_map>
-#include <map>
+#include <unordered_set>
 #include <queue>
-#include <cstdlib>
+#include <stdlib.h>
 
 using namespace std;
 
-
 #define null (-99)
 
-template<typename T>
-void printVector(const T& t) {
-    printf("[");
-    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
-    printf("], ");
-}
-
-template<typename T>
-void printVectorInVector(const T& t) {
-    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
-}
 
 struct ListNode {
     int val;
@@ -32,60 +20,175 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode* createListNode(vector<int> &a) {
-    ListNode *p;
-    ListNode *head, *tail;
-
-    head = new ListNode();
-    head->val = a[0];
-    head->next = nullptr;
-
-    int i;
-    for (i = 1, tail = head; i < a.size(); ++i) {
-        p = new ListNode();
-        p->val = a[i];
-        tail->next = p;
-        tail = p;
-    }
-    tail->next = nullptr;
-    return head;
+template<typename T>
+void printVector(const T& t) {
+    cout << "[";
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
 }
 
+template<typename T>
+void printVectorInVector(const T& t) {
+    std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
 
-void printListNodes(ListNode* head) {
-    for (ListNode* p = head; p != nullptr; p = p->next)
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* createTree(vector<int>& inputs){
+    queue<TreeNode*> my_queue;
+    TreeNode* root = new TreeNode(inputs[0]);
+    my_queue.push(root);
+
+    TreeNode *node;
+    int i = 0;
+    while (!my_queue.empty()) {
+        node = my_queue.front();
+        my_queue.pop();
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+
+        if (inputs[i] != null) {
+            node->left = new TreeNode(inputs[i]);
+            my_queue.push(node->left);
+        }
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+        if (inputs[i] != null) {
+            node->right = new TreeNode(inputs[i]);
+            my_queue.push(node->right);
+        }
+    }
+    return root;
+
+}
+
+struct Trunk
+{
+    Trunk *prev;
+    string str;
+
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
+
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr) {
+        return;
+    }
+
+    showTrunks(p->prev);
+    cout << p->str;
+}
+
+void printTree(TreeNode* root, Trunk *prev, bool isLeft)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(root->right, trunk, true);
+
+    if (!prev) {
+        trunk->str = "---";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "/---";
+        prev_str = "   |";
+    }
+    else {
+        trunk->str = "\\---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << " " << root->val << endl;
+
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "   |";
+
+    printTree(root->left, trunk, false);
+}
+
+ListNode* stringToListNode(vector<int> &list) {
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
         printf("%d -> ", p->val);
-    printf("NULL\n");
+    printf("null\n");
 }
 
 class Solution {
 public:
     int minCut(string s) {
-        int n = (int)s.size();
+        int n = (int) s.size();
+
         int i, j;
 
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        // dp1[i][j] 表示 s[i, j] 是否回文
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        for (i = 0; i < n; ++i)
+            dp[i][i] = true;
+
+        for (i = n - 2; i >= 0; --i)
+            for (j = i + 1; j < n; ++j)
+                if (s[i] == s[j])
+                    dp[i][j] = (j - i == 1) ? true : dp[i + 1][j - 1];
+
+        // dp1[j] 表示 s[0, j] 切几次
         vector<int> dp1(n);
 
-        // record dp1[j]
-        int dp1_result;
-        for (j = 0; j < n; ++j){
-            dp1_result = j;
-            for (i = 0; i <= j; ++i){
-                if (s[i] == s[j] && (j - i < 2 || dp[i+1][j-1])) {
-                    dp[i][j] = true;
-                    dp1_result = i == 0? 0 :min(dp1_result, dp1[i-1] + 1);
-                }
+        for (j = 0; j < n; ++j) {
+            // 最差就是有几个元素，分割成几个字符串（每个字符串就只有一个元素，肯定对称）
+            dp1[j] = j;
+            if (dp[0][j])
+                dp1[j] = 0;
+            else {
+                // 把切割 s[0, j] 分解为 切割 s[0, j] + 剩余为回文，这样两个子任务
+                for (i = 0; i < j; ++i)
+                    if (dp[i + 1][j])
+                        dp1[j] = min(dp1[j], dp1[i] + 1);
             }
-            dp1[j] = dp1_result;
         }
-
-        return dp1[n-1];
+        return dp1[n - 1];
     }
 };
-
-int main(){
-    string s = "ab";
+int main() {
+    string s = "abbab";
     int answer = Solution().minCut(s);
     cout << answer << endl;
 }
