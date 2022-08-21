dd7f53070ffecb57b29eba627b0973c28202bac5
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-08-21 22:02:11 +0800

    Update main1.cpp

--- 0127/main1.cpp
+++ 0127/main1.cpp
@@ -4,9 +4,10 @@
 #include <algorithm>
 #include <unordered_map>
 #include <map>
-#include <set>
 #include <queue>
 #include <cstdlib>
+#include <sstream>
+#include <unordered_set>
 
 using namespace std;
 
@@ -61,65 +62,70 @@ void printListNodes(ListNode* head) {
 
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        set<string> visited_begin, visited_end, setWordList; int i, n, changed_index;
-        for (i = 0; i < wordList.size(); ++i)
-            setWordList.insert(wordList[i]);
+    bool bfs(unordered_set<string> &dictionary, queue<string> &my_queue, unordered_set<string> &visited, unordered_set<string> &visited1){
+        int i;
+        string cur;
+        int cur_level_count;
 
-        // https://leetcode.com/problems/word-ladder/discuss/40711/Two-end-BFS-in-Java-31ms./38593
-        if(setWordList.find(endWord) == setWordList.end()) return 0;
+        cur_level_count = my_queue.size();
+        while (cur_level_count--) {
+            cur = my_queue.front();
+            my_queue.pop();
+
+            for (i = 0; i < cur.size(); ++i) {
+                string cur_copy = cur;
+                for (char c = 'a'; c <= 'z'; ++c) {
+                    cur_copy[i] = c;
+                    if (visited.find(cur_copy) == visited.end() && dictionary.find(cur_copy) != dictionary.end()) {
+                        visited.insert(cur_copy);
+                        my_queue.push(cur_copy);
+                        if (visited1.find(cur_copy) != visited1.end())
+                            return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dictionary;
+        for (string &word: wordList)
+            dictionary.insert(word);
+
+        if(dictionary.find(endWord) == dictionary.end()) return 0;
 
         queue<string> my_queue_begin, my_queue_end;
-        visited_begin.insert(beginWord);my_queue_begin.push(beginWord);
-        visited_end.insert(endWord);my_queue_end.push(endWord);
+        unordered_set<string> visited_begin, visited_end;
 
-        int layer = 0;
-        string cur, cur_copy; char c;
-        // if "||" instead of "&&", then eg: when my_queue_begin is empty, then `n = (int)my_queue_begin.size() = 0` never stop
-        while(!my_queue_begin.empty() && !my_queue_end.empty()){
-            if (my_queue_begin.size() > my_queue_end.size()) {
-                n = (int)my_queue_end.size();
-                ++layer;
-                for (i = 0; i < n; ++i){
-                    cur = my_queue_end.front();
-                    my_queue_end.pop();
+        my_queue_begin.push(beginWord);
+        visited_begin.insert(beginWord);
 
-                    for (changed_index = 0; changed_index < cur.size(); ++changed_index) {
-                        cur_copy = cur;
-                        for (c = 'a'; c <= 'z'; ++c) {
-                            cur_copy[changed_index] = c;
-                            if (setWordList.find(cur_copy) != setWordList.end() && visited_end.find(cur_copy) == visited_end.end()) {
-                                // return when finding visited_begin and visited_end have a same element
-                                if (visited_begin.find(cur_copy) != visited_begin.end())
-                                    return layer + 1;
-                                visited_end.insert(cur_copy);my_queue_end.push(cur_copy);
-                            }
-                        }
-                    }
-                }
+        my_queue_end.push(endWord);
+        visited_end.insert(endWord);
+
+        string cur;
+        int cur_level = 2;
+
+        // 注意这是 &&
+        // if "||" instead of "&&", then eg:
+        // 若 my_queue_begin 是 empty, 则 my_queue_begin.size() <= my_queue_end.size() 一直满足
+        // 但这样会导致一直停不下来
+        // 而且 !my_queue_begin.empty() && !my_queue_end.empty() 也很对啊，一个空了，证明两个队列（两张图）不连通的
+        while (!my_queue_begin.empty() && !my_queue_end.empty()){
+            // 因为这个题目是个 sb，beginWord 可能不在 wordList 里面，所以第一步必须是从 my_queue_begin 开始
+            // 此后所有的单词都在 wordList 里面了
+            if (my_queue_begin.size() <= my_queue_end.size()) {
+                if (bfs(dictionary, my_queue_begin, visited_begin, visited_end))
+                    return cur_level;
+                ++cur_level;
             }
-            else{
-                n = (int)my_queue_begin.size();
-                ++layer;
-                for (i = 0; i < n; ++i){
-                    cur = my_queue_begin.front();
-                    my_queue_begin.pop();
-
-                    for (changed_index = 0; changed_index < cur.size(); ++changed_index) {
-                        cur_copy = cur;
-                        for (c = 'a'; c <= 'z'; ++c) {
-                            cur_copy[changed_index] = c;
-                            if (setWordList.find(cur_copy) != setWordList.end() && visited_begin.find(cur_copy) == visited_begin.end()) {
-                                // return when finding visited_begin and visited_end have a same element
-                                if (visited_end.find(cur_copy) != visited_end.end())
-                                    return layer + 1;
-                                visited_begin.insert(cur_copy);my_queue_begin.push(cur_copy);
-                            }
-                        }
-                    }
-                }
+            else {
+                if (bfs(dictionary, my_queue_end, visited_end, visited_begin))
+                    return cur_level;
+                ++cur_level;
             }
-
         }
         return 0;
     }
