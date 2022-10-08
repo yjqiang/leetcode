54d5f6b401de9837c89e2374e344d5621bcd9009
yjqiang <8900942+yjqiang@users.noreply.github.com>
2022-10-08 09:22:18 +0800

    Update main.cpp

--- 0069/main.cpp
+++ 0069/main.cpp
@@ -4,17 +4,29 @@
 #include <algorithm>
 #include <unordered_map>
 #include <map>
+#include <unordered_set>
 #include <queue>
 #include <stdlib.h>
+#include <set>
 
 using namespace std;
 
-
 #define null (-99)
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 template<typename T>
 void printVector(const T& t) {
+    cout << "[";
     std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "], ";
 }
 
 template<typename T>
@@ -22,42 +34,152 @@ void printVectorInVector(const T& t) {
     std::for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
 }
 
-
-
-class WordTreeNode{
-public:
-    WordTreeNode* children[2];
+struct TreeNode {
     int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
-    WordTreeNode(){
-        children[0] = children[1] = nullptr;
+TreeNode* createTree(vector<int>& inputs){
+    queue<TreeNode*> my_queue;
+    TreeNode* root = new TreeNode(inputs[0]);
+    my_queue.push(root);
+
+    TreeNode *node;
+    int i = 0;
+    while (!my_queue.empty()) {
+        node = my_queue.front();
+        my_queue.pop();
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+
+        if (inputs[i] != null) {
+            node->left = new TreeNode(inputs[i]);
+            my_queue.push(node->left);
+        }
+
+        ++i;
+        if (i >= inputs.size())
+            break;
+        if (inputs[i] != null) {
+            node->right = new TreeNode(inputs[i]);
+            my_queue.push(node->right);
+        }
+    }
+    return root;
+
+}
+
+struct Trunk
+{
+    Trunk *prev;
+    string str;
+
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
     }
 };
 
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr) {
+        return;
+    }
+
+    showTrunks(p->prev);
+    cout << p->str;
+}
+
+void printTree(TreeNode* root, Trunk *prev, bool isLeft)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(root->right, trunk, true);
+
+    if (!prev) {
+        trunk->str = "---";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "/---";
+        prev_str = "   |";
+    }
+    else {
+        trunk->str = "\\---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << " " << root->val << endl;
+
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "   |";
+
+    printTree(root->left, trunk, false);
+}
+
+ListNode* stringToListNode(vector<int> &list) {
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void printListNodes(struct ListNode* head) {
+    int i;
+    struct ListNode* p;
+    for (p = head, i = 0; p != nullptr && i < 20; p = p->next, ++i)
+        printf("%d -> ", p->val);
+    printf("null\n");
+}
 
 class Solution {
 public:
-    // 小于等于某元素的最大值
     int mySqrt(int x) {
-        long long left, right, mid;
-        left = 0;
-        right = x;
-        long long multiply;
+        // 二分查找
+        // eg:
+        // answer: [0, 1, 2, 3, 4, 5]
+        // 平方数 : [0, 1, 4, 9, 16, 25]
+        // 给定 x，我们要找所有小于等于 x 值的“平方数”，最大的那个，对应的 answer 即为所求
+        long long left = 0, right = x;
+        long long mid;
         while (left < right) {
             mid = left + (right - left + 1) / 2;
-            multiply = mid * mid;
-            if (multiply <= x)
+
+            if (mid * mid <= x)
                 left = mid;
             else
-                right = mid - 1;
-
+                right = mid-1;
         }
-        return left;
+        return (int)left;
     }
 };
 
-int main(){
-    int answer = Solution().mySqrt(8);
+int main() {
+    auto answer = Solution().mySqrt(8);
+
     cout << answer << endl;
 
+    return 0;
 }
